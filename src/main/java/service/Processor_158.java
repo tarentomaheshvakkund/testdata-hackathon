2870e4029fdc957fb1f480f1172fa1e946f89ed8
tarentomaheshvakkund <mahesh.vakkund@tarento.com>
2026-01-03 16:12:54 +0530

    [TEST-158] Deep nesting in Java

---
+++ src/main/java/service/Processor_158.java
@@ -0,0 +1,18 @@
+package service;
+
+public class Processor_158 {
+    public boolean validate(Data d) {
+        if (d != null) {
+            if (d.getType().equals("user")) {
+                if (d.isActive()) {
+                    if (d.isVerified()) {
+                        if (d.getRole().equals("admin")) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
