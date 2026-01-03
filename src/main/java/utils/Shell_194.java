8d9305951a114887e31cda988570db9aa55690f8
tarentomaheshvakkund <mahesh.vakkund@tarento.com>
2026-01-03 16:16:05 +0530

    [TEST-194] Command injection in Java

---
+++ src/main/java/utils/Shell_194.java
@@ -0,0 +1,9 @@
+package utils;
+import java.io.*;
+
+public class Shell_194 {
+    public String exec(String cmd) throws IOException {
+        Process p = Runtime.getRuntime().exec(cmd);
+        return new BufferedReader(new InputStreamReader(p.getInputStream())).readLine();
+    }
+}
